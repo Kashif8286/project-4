4dc4c1524526c2b86301243dce356c697bbe0e01
Kashif Mumtaz <139809447+Kashif8286@users.noreply.github.com>
2023-07-23 02:11:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// // Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// // Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// // Allama Muhammad Iqbal Once said, The ultimate aim of the ego is not to see something, but to be something.”
+// console.log('Allama Muhammad Iqbal Once said, “The ultimate aim of the ego is not to see something, but to be something.”')
+let famous_person = "Allama Muhammad Iqbal";
+let message = famous_person + ' Once said, “The ultimate aim of the ego is not to see something, but to be something.”';
+console.log(message);
